f19030830775ae71ae9b66cf89039cc38cd16651
Melissa Hiatt <melissahiatt@gmail.com>
2023-08-30 16:35:35 -0700

    block build

--- blocks/build/button-filter/index.asset.php
+++ blocks/build/button-filter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '0ed0bc1d3fff8f7332e1');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '24fff4b7bf6160e43faf');
--- blocks/build/social-info/index.asset.php
+++ blocks/build/social-info/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd42c7f34d7325316f607');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3c2de8ee6382c8e3d337');
